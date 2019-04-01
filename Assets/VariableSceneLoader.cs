ec0b0c42d88b36c4546d57fa179378a1a6ee13b7
Evolved Bag <undercloudstudio@gmail.com>
2019-04-01 11:10:09 -0700

    Managed to get custom Editor Window to appear
    now just need to fix the code for the options

--- Assets/VariableSceneLoader.cs
+++ Assets/VariableSceneLoader.cs
@@ -5,15 +5,25 @@ using UnityEngine.SceneManagement;
 using UnityEditor;
 
 public class VariableSceneLoader : EditorWindow {
-	 
+	
+	[MenuItem("Window/New Option")]
+	static void Init(){
+		
+		VariableSceneLoader window = (VariableSceneLoader)EditorWindow.GetWindow(typeof(VariableSceneLoader));
+		window.Show();
 
-	void OnGUI(){
+	}
+
+
+
+	static void OnGUI(){
 
 		for(int i = 0; i < SceneManager.sceneCountInBuildSettings; i++){
 
-			if(GUI.Button(new Rect(20,40 + (i * 30),80,20), "Scene " + i))
-			{
+			if(GUILayout.Button("Scene " + i)){
+				
 				SceneManager.LoadScene(i);
+
 			}
 
 		}
