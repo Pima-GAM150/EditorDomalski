d246aff444dade7346ac1bea1ae1dec30e031a64
Evolved Bag <undercloudstudio@gmail.com>
2019-04-01 09:39:51 -0700

    Testing basic idea for script

--- Assets/VariableSceneLoader.cs
+++ Assets/VariableSceneLoader.cs
@@ -5,6 +5,19 @@ using UnityEngine.SceneManagement;
 using UnityEditor;
 
 public class VariableSceneLoader : EditorWindow {
+	 
 
+	void OnGUI(){
+
+		for(int i = 0; i < SceneManager.sceneCountInBuildSettings; i++){
+
+			if(GUI.Button(new Rect(20,40 + (i * 30),80,20), "Scene " + i))
+			{
+				SceneManager.LoadScene(i);
+			}
+
+		}
+
+	}
 
 }
