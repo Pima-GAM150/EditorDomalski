e0513cacf0327877c059be9841eba0e657bdde59
Evolved Bag <undercloudstudio@gmail.com>
2019-04-02 07:22:48 -0700

    Added toggle
    can now choose if you want to save your scenes before loading the next ones

--- Assets/VariableSceneLoader.cs
+++ Assets/VariableSceneLoader.cs
@@ -12,30 +12,45 @@ public class VariableSceneLoader : EditorWindow {
 	[MenuItem("Window/Variable Scene Loader")]
 	static void Init(){
 
-		pain = new List<string>();
-
-		for(int i = 0; i < SceneManager.sceneCountInBuildSettings; i++){
-
-			Debug.Log(SceneUtility.GetScenePathByBuildIndex(i));
-			pain.Add(SceneUtility.GetScenePathByBuildIndex(i));
-
-		}
+		PainInit();
 
 		VariableSceneLoader window = (VariableSceneLoader)EditorWindow.GetWindow(typeof(VariableSceneLoader));
 		window.Show();
 
 	}
 
+	void PainInit(){
+
+		pain = new List<string>();
+
+		for(int i = 0; i < SceneManager.sceneCountInBuildSettings; i++){
+
+			//Debug.Log(SceneUtility.GetScenePathByBuildIndex(i));
+			pain.Add(SceneUtility.GetScenePathByBuildIndex(i));
+
+		}
+
+	}
 
 
 	 void OnGUI(){
 
 		//Debug.Log(SceneManager.sceneCountInBuildSettings);
 		int temp = 0;
+		bool save = EditorGUILayout.Toggle("Save before loading", true);
+		if(pain == null || pain.Count < SceneManager.sceneCountInBuildSettings){ Pain Init(); }
+
 		for(int i = 0; i < SceneManager.sceneCountInBuildSettings; i++){
 
 			if(GUILayout.Button("Scene " + (i+1))){
-				
+
+				if(save){
+
+					EditorSceneManager.MarkAllScenesDirty();
+					EditorSceneManager.SaveOpenScenes();
+
+				}
+
 				EditorSceneManager.OpenScene(pain[temp]);
 
 			}
