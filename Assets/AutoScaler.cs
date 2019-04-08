36926eadd0576ab842542c0be320528524d01de7
Evolved Bag <undercloudstudio@gmail.com>
2019-04-08 14:43:44 -0700

    Window for Auto Scaler appears

--- Assets/AutoScaler.cs
+++ Assets/AutoScaler.cs
@@ -8,7 +8,8 @@ public class AutoScaler : EditorWindow{
 	[MenuItem("Window/Auto Scaler")]
 	static void Init(){
 
-
+		AutoScaler window = (AutoScaler)EditorWindow.GetWindow(typeof(AutoScaler));
+		window.Show();
 
 	}
 
