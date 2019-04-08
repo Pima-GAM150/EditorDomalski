c1d7d125b4bc892117b0a6316499871b957095c4
Evolved Bag <undercloudstudio@gmail.com>
2019-04-08 15:09:20 -0700

    Testing Togglable Float fields

--- Assets/AutoScaler.cs
+++ Assets/AutoScaler.cs
@@ -5,18 +5,33 @@ using UnityEditor;
 
 public class AutoScaler : EditorWindow{
 
+	static Vector3 scaleSet;
+	static bool setx, sety, setz;
+
 	[MenuItem("Window/Auto Scaler")]
 	static void Init(){
 
+		scaleSet = new Vector3();
 		AutoScaler window = (AutoScaler)EditorWindow.GetWindow(typeof(AutoScaler));
 		window.Show();
+
+	}
+
+	static void SetScale(){
+
+
 
 	}
 
    
 	void OnGUI(){
 
+		setx = EditorGUILayout.Toggle("Set X scale", setx);
+		if(setx){
 
+			scaleSet.Set(EditorGUILayout.FloatField("size in X direction", scaleSet.x), scaleSet.y, scaleSet.z);
+
+		}
 
 	}
 
