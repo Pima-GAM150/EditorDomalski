56d70255ff723a072f3abdc90fa4f9a447b1170b
Evolved Bag <undercloudstudio@gmail.com>
2019-04-08 14:31:08 -0700

    Starting work on Auto Scaler window

---
+++ Assets/AutoScaler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AutoScaler : EditorWindow{
+
+	[MenuItem("Window/Auto Scaler")]
+	static void Init(){
+
+
+
+	}
+
+   
+	void OnGUI(){
+
+
+
+	}
+
+}
